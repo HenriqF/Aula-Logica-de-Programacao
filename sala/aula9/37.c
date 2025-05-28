0796eec3aaa919f4a4d010d52a5e37b546887dc5
Henrique Reinaldi <carlosofalmeida@gmail.com>
2025-05-28 18:30:02 -0300

    Add files via upload

---
+++ sala/aula9/37.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main() {
+    int l;
+    double m[12][12], ans=0;
+    char c;
+
+    scanf("%d\n%c", &l, &c);
+    for (int i=0;i<12;i++){
+        for(int j=0;j<12;j++){
+            scanf("%lf", &m[i][j]); 
+            if (i == l){
+                ans += m[i][j];
+            }
+        }
+    }
+
+    if(c == 'M'){
+        printf("%.1lf", ans/12);
+    }
+    else if(c == 'S'){
+        printf("%.1lf", ans);
+    }
+
+    return 0;
+}
